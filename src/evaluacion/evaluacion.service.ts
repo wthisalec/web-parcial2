8b37271dfef3491ce33e55c6c7e3142e673ca705
Alejandro Charry <ja.charry@uniandes.edu.co>
2025-05-15 10:49:22 -0500

    eva y porf

--- src/evaluacion/evaluacion.service.ts
+++ src/evaluacion/evaluacion.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EvaluacionEntity } from 'src/evaluacion.entity/evaluacion.entity';
+import { Repository } from 'typeorm';
 
-@Injectable()
-export class EvaluacionService {}
+export class EvaluacionService {
+
+    constructor(
+            @InjectRepository(EvaluacionEntity)
+            private readonly evaluacionRepository: Repository<EvaluacionEntity>,    
+        ) {}
+
+    async create(evaluacion: EvaluacionEntity): Promise<EvaluacionEntity> {
+            return this.evaluacionRepository.save(evaluacion);
+        }
+}
--- src/profesor/profesor.service.ts
+++ src/profesor/profesor.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProfesorEntity } from 'src/profesor.entity/profesor.entity';
+import { Repository } from 'typeorm';
 
-@Injectable()
-export class ProfesorService {}
+export class ProfesorService {
+    constructor(
+                @InjectRepository(ProfesorEntity)
+                private readonly profesorRepository: Repository<ProfesorEntity>,    
+            ) {}
+    
+    async create(profesor: ProfesorEntity): Promise<ProfesorEntity> {
+                return this.profesorRepository.save(profesor);
+            }
+
+    
+}
